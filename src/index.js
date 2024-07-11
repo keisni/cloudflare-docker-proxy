c94030162ea373a58110c74c382759c4db18431d
zhangming01382 <zhangming01382@zulong.com>
2024-07-11 12:26:11 +0800

    docker acceleration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -6,18 +8,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.keisni.top": "https://registry-1.docker.io",
+  "quay.keisni.top": "https://quay.io",
+  "gcr.keisni.top": "https://gcr.io",
+  "k8s-gcr.keisni.top": "https://k8s.gcr.io",
+  "k8s.keisni.top": "https://registry.k8s.io",
+  "ghcr.keisni.top": "https://ghcr.io",
+  "cloudsmith.keisni.top": "https://docker.cloudsmith.io"
 };
 
 function routeByHosts(host) {
@@ -42,6 +39,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
